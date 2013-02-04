8fabd2bceeda94476d04ca39463943ecf4e08d5b
Axel Campailla <axelcampailla@hotmail.com>
2013-02-04 15:38:22 -0800

    Adding orders detail.

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -10,16 +10,16 @@ class TemplatesController < ApplicationController
 
 	# GET /generate_template
 	# GET /generate_template.json
-	def index
+	def to_mas_so_sales_order_header
  
 		csv_content = ""
 
 		# Copy the csv file localy through HTTP
 		open('orders_export.csv', 'wb') do |file|
-  			csv_content << open('http://betatopa.nextmp.net/orders_export/orders_export.csv').read
+  			csv_content << open('http://betatopa.nextmp.net/orders_export/ToMAS_SO_SalesOrderHeader.csv').read
 		end
 
-		file_name = "orders_export.csv";
+		file_name = "ToMAS_SO_SalesOrderHeader.csv";
 		csv_file = File.open(file_name, "w")
 		csv_file.puts csv_content
 		csv_file.close
@@ -94,6 +94,64 @@ class TemplatesController < ApplicationController
 
 		# print out the number of records
 		puts "Item count: #{com_tomas_so_salesorderhdr.count}"
+ 
+		# Accessing this view launch the service automatically
+		respond_to do |format|
+			format.html # index.html.erb
+		end
+
+	end
+
+
+	def to_mas_so_sales_order_detail
+ 
+		csv_content = ""
+
+		# Copy the csv file localy through HTTP
+		open('ToMas_SO_SalesOrderDetail.csv', 'wb') do |file|
+  			csv_content << open('http://betatopa.nextmp.net/orders_export/ToMas_SO_SalesOrderDetail.csv').read
+		end
+
+		file_name = "ToMas_SO_SalesOrderDetail.csv";
+		csv_file = File.open(file_name, "w")
+		csv_file.puts csv_content
+		csv_file.close
+
+		# Load the source csv file
+		orders_export = import(csv_file) do
+		  read_attributes_from_file
+		end
+		
+
+		orders_line = 2
+
+
+		# Finally, execute the SQL query on the middle-tier database
+		db_connection = Sequel.connect('postgres://zlqmruskgjfmsn:ZL1exeZYZEVN9O9E0qTQ8uKBmX@ec2-23-21-176-133.compute-1.amazonaws.com:5432/dajd8d3f0o9thb')
+
+		# create a dataset from the items table
+		com_tomas_so_salesorderdetl = db_connection[:com_tomas_so_salesorderdetl]
+
+
+		while !orders_export[orders_line].nil? do
+			
+			salesorderno = orders_export[orders_line].salesorderno
+			sequenceno = orders_export[orders_line].sequenceNo
+			itemcode = orders_export[orders_line].itemcode
+			itemtype = orders_export[orders_line].itemtype
+			quantityorderedoriginal = orders_export[orders_line].quantityorderedoriginal
+			originalunitprice = orders_export[orders_line].originalunitprice
+
+			# Populate the table
+			com_tomas_so_salesorderdetl.insert(:salesorderno => salesorderno, :sequenceno => sequenceno, :itemcode => itemcode, :itemtype => itemtype,
+				:quantityorderedoriginal => quantityorderedoriginal, :originalunitprice => originalunitprice)
+
+			orders_line += 1
+
+		end
+
+		# print out the number of records
+		puts "Item count: #{com_tomas_so_salesorderdetl.count}"
  
 		# Accessing this view launch the service automatically
 		respond_to do |format|
