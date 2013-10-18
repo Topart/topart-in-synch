392b0de994aeaac3f18ab910bdff80af32ea0a18
Axel Campailla <axelcampailla@hotmail.com>
2013-10-17 19:09:27 -0700

    Framing/matting/stretching UnitCost integration.

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -221,6 +221,8 @@ class TemplatesController < ApplicationController
   				retail_csv_content << open('http://topartco.nextmp.net/orders_export/retail_master_framing_matting_stretching.csv').read
 
   			end
+
+  			p sku_code
 		end
 
 		retail_csv_file = File.open(retail_file_name, "w")
@@ -355,6 +357,8 @@ class TemplatesController < ApplicationController
 			retail_master = pick_retail_sheet(retail_substrate, border)
 
 			while !retail_master[retail_line].nil? do
+
+				p udf_entitytype
 
 				# If Poster or digital Paper
 				if udf_entitytype == "Poster" or (udf_entitytype == "Image" and substrate == "PR")
